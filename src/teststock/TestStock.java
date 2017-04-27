7ffbc673d8a73d7e71725cf294a10b0d98bfd885
lengphen-ddd <leng.phen@gmail.com>
2017-04-27 23:49:47 +0700

    update

--- src/teststock/TestStock.java
+++ src/teststock/TestStock.java
@@ -15,7 +15,7 @@ public class TestStock {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Testing first file");
     }
     
 }
