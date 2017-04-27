cd85fc2310d0784e95edcb105b94134e2151d82b
lengphen-ddd <leng.phen@gmail.com>
2017-04-28 00:03:53 +0700

    update by phen

---
+++ src/htd/sm/StockManagement.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htd.sm;
+
+/**
+ *
+ * @author ERROR
+ */
+public class StockManagement {
+    public static void main(String agrs[]){
+        System.out.println("Test second file");
+    }
+}
